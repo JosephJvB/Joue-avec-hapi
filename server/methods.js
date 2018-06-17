a96f8d88a967f605d43264a993a682de0cf091fd
JosephJvB <joevanbo@pm.me>
2018-06-18 11:45:55 +1200

    docs(db/server): Add comment

--- server/methods.js
+++ server/methods.js
@@ -1,9 +1,17 @@
 /*
   When this gets more serious, refactor to file structure: server/methods/method-name.js
   but then I need these to talk to the database...
+
+  I think each method could be connected to some knex function
+  const { create, read, update, delete } = require('./db-actions)
+
+  Feathers gives you more than that in that regard - with pre-built patch, create etc functions
+
   Done prev with feathers knex
-  https: //github.com/feathersjs/feathers/issues/258
-  man IDK
+  https: //github.com/feathersjs/feathers/issues/258 - look there's mikey!
+
+  But yolo
+  Still no idea how to export server object for redux thunk
 */
 
 const add = {
